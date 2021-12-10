0392120c5c0e68d89d5a326bc2954465448f1a51
Artur Stambultsian <bumbledos@gmail.com>
2021-12-10 13:42:19 +0300

    modals and dropdowns

--- src/Components/ColorSchemeSwitcher/ColorSchemeSwitcher.tsx
+++ src/Components/ColorSchemeSwitcher/ColorSchemeSwitcher.tsx
@@ -1,6 +1,10 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import { applyScheme, getSavedScheme, getSystemScheme, removeSavedScheme } from '../../colorSchemeUtils';
 import './ColorSchemeSwitcher.css';
+import { Auto } from '@components/Icons/Auto';
+import { Moon } from '@components/Icons/Moon';
+import { Sun } from '@components/Icons/Sun';
+import { Droprown } from '@components/Dropdown/Dropdown';
 
 type ColorSchemeSwitcherValues = 'auto' | 'dark' | 'light';
 
@@ -8,6 +12,8 @@ const matchMedia = window.matchMedia('(prefers-color-scheme:dark)');
 
 export const ColorSchemeSwitcher: FC = () => {
   const [userScheme, setUserScheme] = React.useState<ColorSchemeSwitcherValues>(getSavedScheme() || 'auto');
+  const [dropdownShown, setDropdownShown] = useState(false);
+  const targetRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     if (userScheme === 'auto') {
@@ -32,14 +38,53 @@ export const ColorSchemeSwitcher: FC = () => {
   }, [userScheme]);
 
   return (
-    <select
-      className="color-scheme-switcher"
-      onChange={(e) => setUserScheme(e.target.value as ColorSchemeSwitcherValues)}
-      value={userScheme}
-    >
-      <option value="dark">Dark</option>
-      <option value="light">Light</option>
-      <option value="auto">Auto</option>
-    </select>
+    <div className="color-scheme-switcher">
+      <button
+        className="color-scheme-switcher__value"
+        ref={targetRef}
+        onClick={() => {
+          setDropdownShown(!dropdownShown);
+        }}
+      >
+        {userScheme === 'auto' && <Auto />}
+        {userScheme === 'dark' && <Moon />}
+        {userScheme === 'light' && <Sun />}
+      </button>
+      <Droprown shown={dropdownShown} onShownChange={setDropdownShown} targetRef={targetRef}>
+        <button className="color-scheme-switcher__option" onClick={() => setUserScheme('auto')}>
+          <Auto />
+          <span className="color-scheme-switcher__text">Авто</span>
+          {userScheme === 'auto' && (
+            <img
+              className="color-scheme-switcher__check"
+              src={require('../../images/check.svg')}
+              alt="Выбранная тема"
+            />
+          )}
+        </button>
+        <button className="color-scheme-switcher__option" onClick={() => setUserScheme('light')}>
+          <Sun />
+          <span className="color-scheme-switcher__text">Светлая</span>
+          {userScheme === 'light' && (
+            <img
+              className="color-scheme-switcher__check"
+              src={require('../../images/check.svg')}
+              alt="Выбранная тема"
+            />
+          )}
+        </button>
+        <button className="color-scheme-switcher__option" onClick={() => setUserScheme('dark')}>
+          <Moon />
+          <span className="color-scheme-switcher__text">Темная</span>
+          {userScheme === 'dark' && (
+            <img
+              className="color-scheme-switcher__check"
+              src={require('../../images/check.svg')}
+              alt="Выбранная тема"
+            />
+          )}
+        </button>
+      </Droprown>
+    </div>
   );
 };
--- src/Components/Page/Page.tsx
+++ src/Components/Page/Page.tsx
@@ -1,17 +1,31 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC, Fragment, useState } from 'react';
 import './Page.css';
 import { Navigation } from '../Navigation/Navigation';
 import { Logo } from '../Logo/Logo';
 import { ColorSchemeSwitcher } from '@components/ColorSchemeSwitcher/ColorSchemeSwitcher';
+import { EmailModal } from '@components/EmailModal/EmailModal';
+
+const LS_EMAIL_SHOWN_KEY = 'newsfeed:email_modal_shown';
 
 export const Page: FC = ({ children }) => {
+  const [emailModalShown, setEmailModalShown] = useState(!localStorage.getItem(LS_EMAIL_SHOWN_KEY));
   return (
     <Fragment>
+      {emailModalShown && (
+        <EmailModal
+          onClose={() => {
+            localStorage.setItem(LS_EMAIL_SHOWN_KEY, 'true');
+            setEmailModalShown(false);
+          }}
+        >
+          Hello
+        </EmailModal>
+      )}
       <header className="header">
         <div className="container header__container">
           <Logo />
           <Navigation className="header__navigation" />
-          <div className="header__controls">
+          <div className="header__controls" style={{ transform: 'translateX(0)' }}>
             <ColorSchemeSwitcher />
           </div>
         </div>
