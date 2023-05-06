93ba2a08cb8192f8e68206461d57bb0191739fa6
Alena Fatianova <ravemybody@gmail.com>
2023-05-07 01:44:59 +0500

    Create email modal component

--- src/components/ModalWrapper/ModalWrapper.tsx
+++ src/components/ModalWrapper/ModalWrapper.tsx
@@ -1,58 +1,62 @@
-import React, { FC, HTMLAttributes, useEffect } from 'react'
-import './ModalWrapper.css'
-import { createPortal } from 'react-dom'
-import classNames from 'classnames'
+import React, { FC, HTMLAttributes, useEffect } from 'react';
+import './ModalWrapper.css';
+import { createPortal } from 'react-dom';
+import classNames from 'classnames';
 
 interface ModalWrapperType extends HTMLAttributes<HTMLElement> {
-  alignX?: 'start' | 'center' | 'end'
-  alignY?: 'start' | 'center' | 'end'
-  onModalClose: () => void
+  alignX?: 'start' | 'center' | 'end';
+  alignY?: 'start' | 'center' | 'end';
+  onModalClose: () => void;
 }
 
-export const ModalWrapper: FC<ModalWrapperType> = ({ 
-  children, 
-  alignX = 'center', 
-  alignY = 'center', 
-  className,  
+export const ModalWrapper: FC<ModalWrapperType> = ({
+  children,
+  alignX = 'center',
+  alignY = 'center',
+  className,
   onModalClose,
-  ...rest 
+  ...rest
 }: ModalWrapperType) => {
   useEffect(() => {
-    document.documentElement.classList.add('--prevent-scroll')
+    document.documentElement.classList.add('--prevent-scroll');
 
     return () => {
-      document.documentElement.classList.remove('--prevent-scroll')
-    }
-  }, [])
- 
+      document.documentElement.classList.remove('--prevent-scroll');
+    };
+  }, []);
+
   useEffect(() => {
     const onKeyDownEventListener = (e: KeyboardEvent) => {
       if (e.key === 'Escape') {
-        onModalClose()
+        onModalClose();
       }
-    }
-    document.addEventListener('keydown', onKeyDownEventListener)
+    };
+    document.addEventListener('keydown', onKeyDownEventListener);
 
     return () => {
-      document.removeEventListener('keydown', onKeyDownEventListener)
-    }
-  }, [onModalClose])
+      document.removeEventListener('keydown', onKeyDownEventListener);
+    };
+  }, [onModalClose]);
 
-  return (
-    createPortal(
-      <div className={
-        classNames(
-          'modal-wrapper', 
-          `modal-wrapper--alignY-${alignY}`, 
-          `modal-wrapper--alignX-${alignX}`,
-          className
-        )} 
-        {...rest} 
-        onClick={onModalClose}
+  return createPortal(
+    <div
+      className={classNames(
+        'modal-wrapper',
+        `modal-wrapper--alignY-${alignY}`,
+        `modal-wrapper--alignX-${alignX}`,
+        className
+      )}
+      {...rest}
+      onClick={onModalClose}
+    >
+      <div
+        className="modal-wrapper__children"
+        onKeyDown={(e) => e.stopPropagation()}
+        onClick={(e) => e.stopPropagation()}
       >
-        <div className='modal-wrapper__children' onKeyDown={e => e.stopPropagation()} onClick={(e) => e.stopPropagation()}>{children}</div>
-      </div>,
-      document.getElementById('overlay') as HTMLElement
-    )
-  )
-}
+        {children}
+      </div>
+    </div>,
+    document.getElementById('overlay') as HTMLElement
+  );
+};
--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -3,11 +3,20 @@ import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
 import { EmailModal } from '@components/EmailModal/EmailModal';
 
+const LS_EMAIL_SHOWN_KEY = 'newsfeed:email_modal_shown';
+
 export const Page: React.FC = ({ children }) => {
-  const [emailModalShow, setEmailModalShow] = useState(true)
+  const [emailModalShow, setEmailModalShow] = useState(!localStorage.getItem(LS_EMAIL_SHOWN_KEY));
   return (
     <>
-      {emailModalShow && <EmailModal onModalClose={() => setEmailModalShow(false)}></EmailModal>}
+      {emailModalShow && (
+        <EmailModal
+          onModalClose={() => {
+            localStorage.setItem(LS_EMAIL_SHOWN_KEY, 'true');
+            setEmailModalShow(false);
+          }}
+        />
+      )}
       <Header />
       {children}
       <Footer />
