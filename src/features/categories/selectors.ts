ffbecdb405b7c6b72e06074e4667b8cebed25d61
Alena Fatianova <ravemybody@gmail.com>
2023-05-20 17:14:14 +0500

    Rename folders

--- src/features/categories/selectors.ts
+++ src/features/categories/selectors.ts
@@ -1,4 +1,4 @@
 import { RootState } from '@components/store'
-import { CategoriesType } from '../categories/types'
+import { CategoriesType } from './types'
 
 export const getCategories = (state: RootState): CategoriesType[] => state.categoriesList
