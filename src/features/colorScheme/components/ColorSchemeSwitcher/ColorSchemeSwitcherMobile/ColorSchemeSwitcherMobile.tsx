b6804fbca8d7e5f0b725304ddf606c7e4b016f7d
Alena Fatianova <ravemybody@gmail.com>
2023-05-25 18:51:37 +0500

    Add two color switchers for mobile and desktop instead of one

---
+++ src/features/colorScheme/components/ColorSchemeSwitcher/ColorSchemeSwitcherMobile/ColorSchemeSwitcherMobile.tsx
@@ -0,0 +1,23 @@
+import { useColorScheme } from 'features/colorScheme/hooks'
+import React, { FC } from 'react'
+import { ColorSchemeSwitcherMenu } from '../ColorSchemeSwitcherMenu/ColorSchemeSwitcherMenu'
+import { ColorSchemeSwitcherButton } from '../ColorSchemeSwitcherButton/ColorSchemeSwitcherButton'
+
+interface Props {
+  onClickSchemeButton: () => any
+  isMenuActive: boolean
+}
+
+export const ColorSchemeSwitcherMobile: FC<Props> = ({ onClickSchemeButton, isMenuActive }) => {
+  const { userScheme, setUserScheme } = useColorScheme()
+
+  return (
+    <div className="color-scheme-switcher-mobile">
+      {isMenuActive ? (
+        <ColorSchemeSwitcherMenu selectedScheme={userScheme} onChangeScheme={(scheme) => setUserScheme(scheme)} />
+      ) : (
+        <ColorSchemeSwitcherButton onClick={onClickSchemeButton} scheme={userScheme} />
+      )}
+    </div>
+  )
+}
